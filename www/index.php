8319480ccc63aef996067cf0668237cd33c633a6
hilevsky <31646405+hilevsky@users.noreply.github.com>
2018-01-10 12:31:10 +0300

    Подготовка к работе Настройка Git и Github ...

--- www/index.php
+++ www/index.php
@@ -1,6 +1,11 @@
 <?php
 
-	//Определяем, с ккаким контроллером будем работать
+	//Определяем, с каким контроллером будем работать
     $controllerName = isset($_GET['controller']) ? ucfirst($_GET['controller']) : 'Index';
 
     echo 'Подключаемый php файл (Контроллер) = '. $controllerName . '<br>';
+
+    //Определяем, с какой функцией будем работать
+    $actionName = isset($_GET['action']) ? ucfirst($_GET['action']) : 'index';
+
+    echo 'Функция, формирующая страницу (action, Экшн) = '. $actionName . '<br>';
