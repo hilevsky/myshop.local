a778731d17aaf83b2e9653c2437e64639882c973
hilevsky <31646405+hilevsky@users.noreply.github.com>
2018-01-10 12:19:53 +0300

    Подготовка к работе Настройка Git и Github ...

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -3,3 +3,9 @@
  * Created 10.01.2018 12:01
  * by E. Hilevsky
  */
+
+/*Тестовая функция, которая выводит,
+из какого контроллера какая функция была вызвана*/
+function testAction(){
+    echo 'IndexController.php > testAction';
+}
--- www/index.php
+++ www/index.php
@@ -1,3 +1,6 @@
 <?php
 
-	phpinfo();
+	//Определяем, с ккаким контроллером будем работать
+    $controllerName = isset($_GET['controller']) ? ucfirst($_GET['controller']) : 'Index';
+
+    echo 'Подключаемый php файл (Контроллер) = '. $controllerName . '<br>';
