2c8c413a76f8de58e911bcd57866c6372b8b03ee
hilevsky <31646405+hilevsky@users.noreply.github.com>
2018-01-10 13:43:10 +0300

    Пишем правильные комментарии

--- config/config.php
+++ config/config.php
@@ -3,6 +3,13 @@
  * Created 10.01.2018 13:17 by E. Hilevsky
  */
 
+
+/**
+ *
+ * ФАЙЛ НАСТРОЕК
+ *
+ */
+
 // Константы для обращения к контроллерам
 define  ('PathPrefix', '../controllers/');
 define('PathPostfix', 'Controller.php');
--- library/mainFunctions.php
+++ library/mainFunctions.php
@@ -3,7 +3,18 @@
  * Created 10.01.2018 13:20 by E. Hilevsky
  */
 
+/**
+ *
+ * ОСНОВНЫЕ ФУНКЦИИ
+ *
+ */
 
+/**
+ * Формироввание запрашиваемой страницы
+ *
+ * @param string $controllerName            название контроллера
+ * @param string $actionName                название функции обработки страницы
+ */
 function loadPage($controllerName, $actionName = 'index'){
     include_once PathPrefix.$controllerName.PathPostfix;
 
