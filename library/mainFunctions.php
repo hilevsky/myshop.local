05645ebc0144d7cfce11a502a1b2c12bf8f3361c
hilevsky <31646405+hilevsky@users.noreply.github.com>
2018-01-12 22:35:31 +0300

    Вывод подпунктов меню, названия подпунктов из подкатегорий из базы данных

--- library/mainFunctions.php
+++ library/mainFunctions.php
@@ -32,3 +32,35 @@ function loadTemplate($smarty, $templateName){
 
     $smarty->display($templateName.TemplatePostfix);
 }
+
+/**
+ * Функция для отладки. Останавливает работу программы,
+ * выводя значение переменной $value
+ *
+ * @param variant $value  -- переменная, которую необходимо
+ * проконтролировать и вывести
+ */
+
+function d($value = null, $die = 1){
+    echo 'Debug: <br><pre>';
+    print_r ($value);
+    echo '</pre>';
+
+    if ($die) die;
+}
+
+/**
+ * Преобразование выборки из БД в ассоциативный массив
+ *
+ * @param recordset $rs -- набор строк (результат работы SELECT)
+ * @return array
+ */
+
+function createSmartyRsArray ($rs){
+    if (! $rs) return false;
+    $smartyRs = array();
+    while ($row = mysqli_fetch_assoc($rs)){
+        $smartyRs[] = $row;
+    }
+    return $smartyRs;
+}
--- models/CategoriesModel.php
+++ models/CategoriesModel.php
@@ -8,8 +8,33 @@
  *
  */
 
-include_once '../config/db.php';
+/**
+ * Получение дочерних категорий для категории $catId
+ *
+ * @param integer $catId   -- ID категории
+ * @return array  -- массив дочерних категорий
+ */
 
+function getChildrenForCat($catId){
+    $sql = "SELECT id, parent_id, name
+            FROM categories
+            WHERE parent_id='{$catId}'";
+
+    global $db;
+
+    $rs = mysqli_query($db, $sql);
+
+    return createSmartyRsArray ($rs);       //Преобразуем результат запроса в массив
+}
+
+
+
+
+/**
+ * Получить главные категории товаров с привязкой дочерних
+ *
+ * @return array массив категорий
+ */
 
 function getAllMainCatsWithChildren(){
     $sql = 'SELECT id, parent_id, name
@@ -21,6 +46,12 @@ function getAllMainCatsWithChildren(){
     $rs = mysqli_query($db, $sql);
 
    while($row = mysqli_fetch_assoc($rs)){
+
+       $rsChildren = getChildrenForCat($row['id']);
+       if($rsChildren){
+           $row['children'] = $rsChildren;
+       }
+
         $smartyRs[] = $row;
     }
     return $smartyRs;
